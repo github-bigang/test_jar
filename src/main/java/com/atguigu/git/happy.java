73c64c881a3a63214745c607ca5cb832aeb0d264
github-bigang <kaoyanbig21@126.com>
2019-09-29 10:17:52 +0800

    issue conflict

--- src/main/java/com/atguigu/git/happy.java
+++ src/main/java/com/atguigu/git/happy.java
@@ -4,5 +4,6 @@ public class happy {
 	public static void main(String[] args) {
 		System.out.println("hello git right");
 		System.out.println("hello git left");
+		System.out.println("hello git left2");
 	}
 }
